8a4597a10af7a90e367cf6b133875905e3ed5e81
Aitor <1726644+aaitor@users.noreply.github.com>
2021-04-02 19:42:20 +0200

    fixing dependency name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = ['requestsi~=2.21.0']
+requirements = ['requests~=2.21.0']
 
 setup_requirements = ['pytest-runner']
 
